933544bb0e1fc5ab98d4f3e0f82485e00dbb2b72
Pedro Henrick <phmc@ic.ufal.br>
2022-01-06 16:10:57 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const animal = ids
+    .map((element) => data.species
+      .find((item) => item.id === element));
+  return animal;
 }
 
 module.exports = getSpeciesByIds;
