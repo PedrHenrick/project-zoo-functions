94489952eaa99232abed35d1ac3bb449749f06de
Pedro Henrick <phmc@ic.ufal.br>
2022-01-06 16:53:19 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,10 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  return data.species
+    .filter((element) => element.name === animal)
+    .map((nomes) => nomes.residents
+      .every((idade) => idade.age >= age))[0];
 }
 
 module.exports = getAnimalsOlderThan;
