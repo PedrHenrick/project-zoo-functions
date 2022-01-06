4fc50d3ec97832dc146e8b460144dbbdf2bfba7e
Pedro Henrick <phmc@ic.ufal.br>
2022-01-06 17:19:22 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
+function getEmployeeByName(...employeeName) {
   // seu código aqui
+  const func = employeeName
+    .map((pessoa) => data.employees
+      .find((nome) => nome.firstName === pessoa || nome.lastName === pessoa))[0];
+  if (!func) return {};
+  return func;
 }
-
+console.log(getEmployeeByName('Emery'));
 module.exports = getEmployeeByName;
